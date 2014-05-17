a19f4575220088bb367ddc935b33773ae84b6b1f
Sam <samhaliburton@gmail.com>
2014-05-17 19:00:37 -0300

    woops

--- Assets/Scripts/StateMachines/ControlsStateMachine/ControlWaitState.cs
+++ Assets/Scripts/StateMachines/ControlsStateMachine/ControlWaitState.cs
@@ -6,7 +6,8 @@ public class ControlWaitState : FSMState<PlayerControls> {
 	
 	public override void Enter(PlayerControls c)
 	{
-
+		Time.timeScale = 1f;
+		Time.fixedDeltaTime = 0.02f*Time.timeScale;
 	}
 	
 	public override void Execute(PlayerControls c)
